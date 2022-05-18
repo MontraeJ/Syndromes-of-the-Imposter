c363c519f8ee9c6215b91192f1886e176509c910
unknown <montrae@montraejemison.com>
2022-05-17 20:18:52 -0500

    Connected to MongoDb

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const connectDb = require("./db/db");
-const usersRouter = require("./routes/users");
+//const usersRouter = require("./routes/users");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -9,9 +9,9 @@ connectDb();
 
 app.use(cors());
 app.use(express.json());
-app.use(`/api/users`, usersRouter);
+//app.use(`/api/users`, usersRouter);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server started on port: ${port}`);
-});
+app.listen(), () => {
+  console.log("Server started on port: ${port}");
+};
