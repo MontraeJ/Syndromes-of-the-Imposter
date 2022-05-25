a9f30dad8e589eaa61cf91f25ca7f2f0ff26c42d
unknown <montrae@montraejemison.com>
2022-05-25 06:36:19 -0500

    check it

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>SYNDROMES OF THE IMPOSTER</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+import {About} from "./AboutPage/about-page";
+import {Forum} from "./ForumPage/forum-page";
+import {Home} from "./HomePage/home-page";
+import {Login} from "./LoginPage/login-page";
+import {SiteRules} from "./SiteRulesPage/site-rules-page";
+
+export{About, Forum, Home, Login, SiteRules};
